92637752a5dea4d5dc09635f957467f9578c8a47
weterynarzfred <weterynarzfred@gmail.com>
2024-05-14 20:29:27 +0200

    setup

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php include 'header.php'; ?>
+<div class="accent">ogrody nowego internetu</div>
+<div class="sub-accent">platformowe ogrody</div>
+<div class="text">
+  <p>Dzisiaj trudno jest zorganizować "strajk dla nowej sieci" nie tylko ze
+    względu na hegemonię kulturową Doliny Krzemowej i jej marketing. Brakuje nam
+    przede wszystkim nowego sposobu myślenia i działania, który mógłby tę
+    hegemonię przełamać. W swoim wystąpieniu będę przekonywał, że uzupełnienie
+    informatyki teoretycznej o wątki związane z ekologią techniki i etyką
+    projektowania może utorować praktyczną drogę prowadzącą do takiego
+    przełamania i ustanowić dojrzałą kulturę techniczną. Jej ustanowienie to
+    jedno z kluczowych wyzwań, przed którym dzisiaj stajemy. Zwłaszcza po tym,
+    gdy informatyczna puszka Pandory z wielkimi modelami językowymi została
+    otwarta.</p>
+</div>
 <?php include 'footer.php'; ?>
