ae94c8aa3e886e8060ac0f18d26fd817fc4cc6f5
weterynarzfred <weterynarzfred@gmail.com>
2024-05-14 19:39:12 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+define("IS_SSL_ENABLED", isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on');
+define(
+  "BASE_URL", (IS_SSL_ENABLED ? "https" : "http") . "://" .
+  $_SERVER['HTTP_HOST'] .
+  str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(__DIR__))
+);
+define(
+  "CURRENT_URL", (IS_SSL_ENABLED ? "https" : "http") . "://" .
+  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
+);
+
+$description = "";
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- <title>tytuł</title> -->
+  <!-- <link rel="icon" type="image/png" href="./media/favicon.png" /> -->
+  <!-- <meta name="description" content="<?= $description ?>" /> -->
+  <meta property="og:url" content="<?= CURRENT_URL ?>" />
+  <meta property="og:type" content="website" />
+  <!-- <meta property="og:title" content="tytuł" /> -->
+  <!-- <meta property="og:description" content="<?= $description ?>" /> -->
+  <!-- <meta property="og:image" content="<?= BASE_URL ?>/media/ogimg.jpg" /> -->
+  <!-- <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" /> -->
+  <meta name="color-scheme" content="light only">
+  <link rel="stylesheet" href="./dist/main.css" />
+</head>
+
+<body>
+  <?php include 'include/partials.php'; ?>
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+// import 'lazysizes';
+// import smoothscroll from 'smoothscroll-polyfill';
+
+import 'widok';
+
+// smoothscroll.polyfill();
+
+// document.addEventListener('lazybeforeunveil', event => {
+//   const bg = event.target.getAttribute('data-bg');
+//   if (bg) event.target.style.backgroundImage = 'url(' + bg + ')';
+// });
