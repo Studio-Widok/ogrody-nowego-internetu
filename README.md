74130fa8b59d97717542bd89eb5d8900830528d9
weterynarzfred <weterynarzfred@gmail.com>
2024-05-15 13:02:39 +0200

    front top info

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 [figma](https://www.figma.com/design/pCSP39Jy2U1znOhgDJ6PJ5/Eden-2024-www?node-id=0-1&t=3b5dXbIMdRNcQ5iY-0)
 
-## todo
+## todo (4/25)
 - [ ] navigation
 - [ ] footer
 - [ ] front
-  - [ ] top
+  - [x] top
     - [x] center
     - [x] background
-    - [ ] bottom
+    - [x] info
   - [ ] images
   - [ ] text
 - [ ] program
--- parts/front-top.php
+++ parts/front-top.php
@@ -33,12 +33,27 @@
       </div>
     </div>
   </div>
+
+  <div></div>
+
   <div id="front-center">
     <div id="front-logo" class="accent">
       ogrody<br>now<span class="alt">e</span>go<br>int<span
         class="alt">e</span>rn<span class="alt">e</span>tu
     </div>
-
     <button class="button">kup bilet</button>
+  </div>
+
+  <div id="front-top-info">
+    <div class="logo-row flex">
+      <img src="./media/logo-apteka.png" alt="apteka designu" class="logo">
+      <img src="./media/logo-mvu.png" alt="minimum viable utopia" class="logo">
+      <img src="./media/logo-grupa-robocza.png" alt="grupa robocza"
+        class="logo">
+    </div>
+    <div class="info-text uppercase">15.06.2024<br>
+      apteka designu<br>
+      kopernika 19a<br>
+      KRAKÓW</div>
   </div>
 </div>
