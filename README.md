fb97b931daf7ce32f53b9185a76575212d08b83f
weterynarzfred <weterynarzfred@gmail.com>
2024-05-15 13:54:16 +0200

    front content

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 [figma](https://www.figma.com/design/pCSP39Jy2U1znOhgDJ6PJ5/Eden-2024-www?node-id=0-1&t=3b5dXbIMdRNcQ5iY-0)
 
-## todo (5/25)
+## todo (8/25)
 - [x] navigation
 - [ ] footer
-- [ ] front
+- [x] front
   - [x] top
     - [x] center
     - [x] background
     - [x] info
-  - [ ] images
-  - [ ] text
+  - [x] images
+  - [x] text
 - [ ] program
   - [ ] top
   - [ ] list
---
+++ parts/front-content.php
@@ -0,0 +1,60 @@
+<div id="front-content">
+
+  <div class="bg-images">
+    <div class="bg-image bg-image--c2">
+      <div class="bg-image__cutoff">
+        <img src="./media/image1.jpg" alt="">
+      </div>
+      <div class="bg-image__caption">
+        <div>IMG.#05</div>
+      </div>
+    </div>
+    <div class="bg-image bg-image--c2">
+      <div class="bg-image__cutoff">
+        <img src="./media/image2.jpg" alt="">
+      </div>
+      <div class="bg-image__caption">
+        <div>IMG.#06</div>
+      </div>
+    </div>
+    <div class="bg-image bg-image--c5">
+      <div class="bg-image__cutoff">
+        <img src="./media/image3.jpg" alt="">
+      </div>
+      <div class="bg-image__caption">
+        <div>IMG.#07</div>
+      </div>
+    </div>
+    <div class="bg-image bg-image--c5">
+      <div class="bg-image__cutoff">
+        <img src="./media/image4.jpg" alt="">
+      </div>
+      <div class="bg-image__caption">
+        <div>IMG.#08</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="content column">
+    <div class="text">
+      <div class="title">opis</div>
+      <p>Internet zmienia się na naszych oczach. Modele sztucznej inteligencji
+        podkręcają tempo transformacji i&nbsp;jednocześnie utrwalają obecny
+        stan rzeczy. Platformowe monokultury rosną w&nbsp;siłę
+        i&nbsp;wpływają na każdy aspekt naszej codzienności. Technologie nas
+        zachwycają i&nbsp;wprawiają w&nbsp;zakłopotanie.</p>
+      <p><strong>
+          Spotykamy się w&nbsp;sobotę 15&nbsp;czerwca w&nbsp;Aptece Designu na
+          Wesołej w&nbsp;Krakowie, żeby złapać oddech i&nbsp;zyskać nową
+          perspektywę.
+        </strong></p>
+      <p>Tegoroczna edycja edenu będzie krążyć wokół zagadnień sztucznej
+        inteligencji i&nbsp;platform społecznościowych. Przyjrzymy się
+        alternatywom dla świata renderowanego przez gigantów technologicznych.
+        Poznamy przekorne, czasem wywrotowe, teorie i&nbsp;praktyki partycypacji
+        w&nbsp;rzeczywistości cyfrowej i&nbsp;wspólnie zastanowimy się,
+        dokąd to wszystko zmierza i&nbsp;czy warto tam iść.</p>
+      <p>Poznaj Internet, o&nbsp;jakim ci się nie śniło.</p>
+    </div>
+  </div>
+</div>
