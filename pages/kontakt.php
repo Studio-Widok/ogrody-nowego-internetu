4ec3018d7fe6601b01f4a58b184f945e32b29360
weterynarzfred <weterynarzfred@gmail.com>
2024-05-15 15:30:55 +0200

    navigation fixes

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -1,8 +1,8 @@
 <?php include '../header.php'; ?>
-<?php get_part('nav', ['current' => 'program']); ?>
+<?php get_part('nav', ['current' => 'kontakt']); ?>
 <div class="rsep"></div>
 <div class="content column">
-  <div class="title accent">program</div>
+  <div class="title accent">kontakt</div>
 </div>
 <div class="rsep"></div>
 <?php get_part('footer'); ?>
--- parts/nav.php
+++ parts/nav.php
@@ -7,16 +7,18 @@ $current  = $current ?? '';
   <div id="nav-wrap">
     <div id="nav-logo" class="text-logo accent">
       <?php if ($showLogo) { ?>
-        ogrody<br>now<span class="alt">e</span>go<br>int<span
-          class="alt">e</span>rn<span class="alt">e</span>tu
+        <a href="<?= BASE_URL ?>">
+          ogrody<br>now<span class="alt">e</span>go<br>int<span
+            class="alt">e</span>rn<span class="alt">e</span>tu
+        </a>
       <?php } ?>
     </div>
     <div id="nav-links">
-      <a
+      <a href="<?= BASE_URL ?>/program"
         class="nav-link <?= $current === 'program' ? 'nav-link--current' : '' ?>">program</a>
-      <a
+      <a href="<?= BASE_URL ?>/prelegenci"
         class="nav-link <?= $current === 'prelegenci' ? 'nav-link--current' : '' ?>">prelegenci</a>
-      <a
+      <a href="<?= BASE_URL ?>/kontakt"
         class="nav-link <?= $current === 'kontakt' ? 'nav-link--current' : '' ?>">kontakt</a>
     </div>
   </div>
