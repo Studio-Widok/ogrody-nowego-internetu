f64e7e74748ef0a0c5bf887635d8f75f030e14f6
weterynarzfred <weterynarzfred@gmail.com>
2024-05-16 20:57:13 +0200

    mobile fixes

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -1,76 +1,79 @@
 <?php include '../header.php'; ?>
-<?php get_part('nav', ['current' => 'kontakt']); ?>
-<div class="rsep"></div>
-<div class="rsep"></div>
-<div class="content column" id="kontakt">
+<div id="kontakt">
+  <?php get_part('nav', ['current' => 'kontakt']); ?>
+  <div class="rsep"></div>
+  <div class="rsep"></div>
+  <div class="content column">
 
-  <div class="bg-images">
-    <div class="bg-image bg-image--c2">
-      <div class="bg-image__cutoff">
-        <img src="./media/image2.jpg" alt="">
-      </div>
-      <div class="bg-image__caption">
-        <div>IMG.#11</div>
+    <div class="bg-images">
+      <div class="bg-image bg-image--c2">
+        <div class="bg-image__cutoff">
+          <img src="./media/image2.jpg" alt="">
+        </div>
+        <div class="bg-image__caption">
+          <div>IMG.#11</div>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="title accent">kontakt</div>
-  <div class="r"></div>
-  <div class="text">
-    <p>
-      Fundacja Grupa Robocza<br>
-      <a href="mailto:grupa@robocza.org"
-        class="fancy-link">grupa@robocza.org</a><br>
-      <a href="https://www.grupa.robocza.org" class="fancy-link" target="_blank"
-        rel="noopener noreferrer">grupa.robocza.org</a><br>
-      <a href="https://www.instagram.com/grupa.robocza/" class="fancy-link"
-        target="_blank" rel="noopener noreferrer">instagram</a><br>
-      <a href="https://mvu.pl" class="fancy-link" target="_blank"
-        rel="noopener noreferrer">mvu.pl</a>
-    </p>
-  </div>
+    <div class="title accent">kontakt</div>
+    <div class="r"></div>
+    <div class="text">
+      <p>
+        Fundacja Grupa Robocza<br>
+        <a href="mailto:grupa@robocza.org"
+          class="fancy-link">grupa@robocza.org</a><br>
+        <a href="https://www.grupa.robocza.org" class="fancy-link"
+          target="_blank" rel="noopener noreferrer">grupa.robocza.org</a><br>
+        <a href="https://www.instagram.com/grupa.robocza/" class="fancy-link"
+          target="_blank" rel="noopener noreferrer">instagram</a><br>
+        <a href="https://mvu.pl" class="fancy-link" target="_blank"
+          rel="noopener noreferrer">mvu.pl</a>
+      </p>
+    </div>
 
+    <div class="rsep"></div>
+    <div class="title accent">grupa robocza</div>
+    <div class="r"></div>
+    <div class="text">
+      <p>
+        <b>Organizatorzy</b><br>
+        Agnieszka Zgud<br>
+        <a
+          href="mailto:agnieszka.zgud@robocza.org">agnieszka.zgud@robocza.org</a>
+      </p>
+
+      <p>
+        Kuba Kulesza<br>
+        <a href="mailto:kuba.kulesza@robocza.org">kuba.kulesza@robocza.org</a>
+      </p>
+
+      <p>
+        <b>Art Direction</b><br>
+        Dawid Prząda<br>
+        Maja Starakiewicz
+      </p>
+
+      <p>
+        <b>Design Support</b><br>
+        Katarzyna Janota
+      </p>
+
+      <p>
+        Web Development<br>
+        Cezary Stępkowski<br>
+        Bartek (Studio Widok)
+      </p>
+
+      <p>
+        Social Media<br>
+        Piotr Dziadowicz (GRUPA ROBOCZA)
+      </p>
+    </div>
+
+  </div>
   <div class="rsep"></div>
-  <div class="title accent">grupa robocza</div>
-  <div class="r"></div>
-  <div class="text">
-    <p>
-      <b>Organizatorzy</b><br>
-      Agnieszka Zgud<br>
-      <a href="mailto:agnieszka.zgud@robocza.org">agnieszka.zgud@robocza.org</a>
-    </p>
-
-    <p>
-      Kuba Kulesza<br>
-      <a href="mailto:kuba.kulesza@robocza.org">kuba.kulesza@robocza.org</a>
-    </p>
-
-    <p>
-      <b>Art Direction</b><br>
-      Dawid Prząda<br>
-      Maja Starakiewicz
-    </p>
-
-    <p>
-      <b>Design Support</b><br>
-      Katarzyna Janota
-    </p>
-
-    <p>
-      Web Development<br>
-      Cezary Stępkowski<br>
-      Bartek (Studio Widok)
-    </p>
-
-    <p>
-      Social Media<br>
-      Piotr Dziadowicz (GRUPA ROBOCZA)
-    </p>
-  </div>
-
+  <div class="rsep"></div>
+  <?php get_part('footer'); ?>
 </div>
-<div class="rsep"></div>
-<div class="rsep"></div>
-<?php get_part('footer'); ?>
 <?php include '../footer.php'; ?>
